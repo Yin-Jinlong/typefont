ce22387aefddf06d13e99b3d356381a0dec322ca
YJL <yjl_1567@qq.com>
2025-06-22 18:26:31 +0800

    feat: 添加ArrayReader

---
+++ typefont/src/io/array_reader.rs
@@ -0,0 +1,67 @@
+use crate::io::error::IOError;
+use crate::io::reader::Reader;
+use std::cmp::min;
+
+pub struct ArrayReader {
+    data: Vec<u8>,
+    pos: usize,
+    mark: usize,
+}
+
+impl ArrayReader {
+    pub fn new(data: &[u8]) -> Result<ArrayReader, std::io::Error> {
+        Ok(Self {
+            data: data.to_vec(),
+            pos: 0,
+            mark: 0,
+        })
+    }
+}
+
+impl From<Vec<u8>> for ArrayReader {
+    fn from(data: Vec<u8>) -> Self {
+        Self {
+            data,
+            pos: 0,
+            mark: 0,
+        }
+    }
+}
+
+impl Reader for ArrayReader {
+    fn position(&self) -> usize {
+        self.pos
+    }
+
+    fn size(&self) -> usize {
+        self.data.len()
+    }
+
+    fn mark(&mut self) -> crate::io::reader::Result<()> {
+        self.mark = self.pos;
+        Ok(())
+    }
+
+    fn get_mark(&self) -> usize {
+        self.mark
+    }
+
+    fn reset(&mut self) -> crate::io::reader::Result<()> {
+        self.seek(self.mark)
+    }
+
+    fn seek(&mut self, pos: usize) -> crate::io::reader::Result<()> {
+        let at = min(self.size(), pos);
+        self.pos = at;
+        Ok(())
+    }
+
+    fn read_bytes(&mut self, len: usize) -> crate::io::reader::Result<Vec<u8>> {
+        if self.eof() {
+            return Err(IOError::UnexpectedEof);
+        }
+        let read_len = min(len, self.remaining());
+        self.pos += read_len;
+        Ok(self.data[self.pos..read_len].to_vec())
+    }
+}
--- typefont/src/io/mod.rs
+++ typefont/src/io/mod.rs
@@ -1,7 +1,7 @@
 pub mod error;
 pub mod file_reader;
 pub mod reader;
-
+pub mod array_reader;
 #[cfg(feature = "writer")]
 pub mod file_writer;
 #[cfg(feature = "writer")]
